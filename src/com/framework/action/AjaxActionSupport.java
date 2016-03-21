a68b4fbc5e0a51094c44c73add4dce94750bb215
unknown <VonChenPlus@gmail.com>
2016-03-22 00:03:17 +0800

    revert AjaxActionSupport changes

--- src/com/framework/action/AjaxActionSupport.java
+++ src/com/framework/action/AjaxActionSupport.java
@@ -1,38 +1,19 @@
 package com.framework.action;
 
+import com.framework.utils.StringUtils;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
-import com.framework.utils.StringUtils;
 import net.sf.json.JSONObject;
 import org.apache.struts2.ServletActionContext;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.Map;
 
-interface UserSession
-{
-    public HttpServletResponse getResponse();
-    public HttpServletRequest getRequest();
-    public String getUserID();
-    public void setUserID(String userID);
-    public String getUserName();
-    public void setUserName(String userName);
-    public String getAttribute(String key);
-    public void setAttribute(String key, Object value);
-    public void removeAttribute(String key);
-}
-
-public abstract class AjaxActionSupport extends ActionSupport implements UserSession {
+public class AjaxActionSupport extends ActionSupport {
     private final static String AJAXACTIONCOMPLETED = "ajaxActionCompleted";
     private String ajaxActionResult;
     private Map parameterMap;
-    private ArrayList<String> skipAction = new ArrayList<String>() {{
-        add("user!login");
-        add("user!logout");
-        add("auth!generateverifycode");
-    }};
 
     public HttpServletResponse getResponse() {
         return (HttpServletResponse) ActionContext.getContext().get(ServletActionContext.HTTP_RESPONSE);
@@ -40,28 +21,6 @@ public abstract class AjaxActionSupport extends ActionSupport implements UserSes
 
     public HttpServletRequest getRequest() {
         return (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
-    }
-
-    public String getUserID() {
-        if (getRequest().getSession(false) != null) {
-            return StringUtils.convertNullableString(getRequest().getSession(false).getAttribute("userID"));
-        }
-        return new String();
-    }
-
-    public void setUserID(String userID) {
-        getRequest().getSession(true).setAttribute("userID", userID);
-    }
-
-    public String getUserName() {
-        if (getRequest().getSession(false) != null) {
-            return StringUtils.convertNullableString(getRequest().getSession(false).getAttribute("userName"));
-        }
-        return new String();
-    }
-
-    public void setUserName(String userName) {
-        getRequest().getSession(true).setAttribute("userName", userName);
     }
 
     public String getAttribute(String key) {
@@ -84,9 +43,6 @@ public abstract class AjaxActionSupport extends ActionSupport implements UserSes
     public void validate() {
         // lazyvalidate
         ActionContext context = ActionContext.getContext();
-        String actionName = context.getName();
-        if (getUserID().isEmpty() && !skipAction.contains(actionName.toLowerCase())) {
-        }
         parameterMap = context.getParameters();
     }
 
