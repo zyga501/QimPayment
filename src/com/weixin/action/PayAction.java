c93240563d46fee865d8c9f3896014db53e05461
unknown <VonChenPlus@gmail.com>
2016-04-03 22:07:30 +0800

    update weixin api

--- src/com/weixin/action/PayAction.java
+++ src/com/weixin/action/PayAction.java
@@ -80,17 +80,22 @@ public class PayAction extends AjaxActionSupport {
 
     public void prePay() throws IOException {
         String appid = new String();
-        String subMerchantId = new String();
-        SubMerchantInfo subMerchantInfo = SubMerchantInfo.getSubMerchantInfoBySubId(getParameter("subMerchantId").toString());
-        if (subMerchantInfo != null) {
-            MerchantInfo merchantInfo = MerchantInfo.getMerchantInfoById(subMerchantInfo.getMerchantId());
-            if (merchantInfo != null) {
-                    appid = merchantInfo.getAppid();
-                    subMerchantId = subMerchantInfo.getSubId();
+        String subMerchantUserId = new String();
+        if (getParameter("id") != null) {
+            subMerchantUserId = getParameter("id").toString();
+            SubMerchantUser subMerchantUser = SubMerchantUser.getSubMerchantUserById(Long.parseLong(subMerchantUserId));
+            if (subMerchantUser != null) {
+                SubMerchantInfo subMerchantInfo = SubMerchantInfo.getSubMerchantInfoById(subMerchantUser.getSubMerchantId());
+                if (subMerchantInfo != null) {
+                    MerchantInfo merchantInfo = MerchantInfo.getMerchantInfoById(subMerchantInfo.getMerchantId());
+                    if (merchantInfo != null) {
+                        appid = merchantInfo.getAppid();
+                    }
                 }
+            }
         }
         else { // compatible old api
-            String subMerchantUserId = getParameter("odod").toString();
+            subMerchantUserId = getParameter("odod").toString();
             // TODO
         }
 
@@ -102,45 +107,48 @@ public class PayAction extends AjaxActionSupport {
         String redirect_uri = getRequest().getRequestURL().substring(0, getRequest().getRequestURL().lastIndexOf("/") + 1) + "index.jsp";
         String perPayUri = String.format("https://open.weixin.qq.com/connect/oauth2/authorize?appid=" +
                 "%s&redirect_uri=%s&response_type=code&scope=snsapi_base&state=%s#wechat_redirect",
-                appid, redirect_uri, subMerchantId);
+                appid, redirect_uri, subMerchantUserId);
         getResponse().sendRedirect(perPayUri);
     }
 
     public String brandWCPay() throws Exception {
-        String subMerchantId = getParameter("state").toString();
+        String subMerchantUserId = getParameter("state").toString();
         String code = getParameter("code").toString();
-        if (subMerchantId.isEmpty() || code.isEmpty()) {
+        if (subMerchantUserId.isEmpty() || code.isEmpty()) {
             return AjaxActionComplete();
         }
 
-        SubMerchantInfo subMerchantInfo = SubMerchantInfo.getSubMerchantInfoBySubId(subMerchantId);
-        if (subMerchantInfo != null) {
-            MerchantInfo merchantInfo = MerchantInfo.getMerchantInfoById(subMerchantInfo.getMerchantId());
-            if (merchantInfo != null) {
-                UnifiedOrderRequestData unifiedOrderRequestData = new UnifiedOrderRequestData();
-                unifiedOrderRequestData.appid = merchantInfo.getAppid();
-                unifiedOrderRequestData.mch_id = merchantInfo.getMchId();
-                unifiedOrderRequestData.sub_mch_id = subMerchantInfo.getSubId();
-                unifiedOrderRequestData.body = getParameter("body").toString();
-                unifiedOrderRequestData.total_fee = Integer.parseInt(getParameter("total_fee").toString());
-                unifiedOrderRequestData.trade_type = "JSAPI";
-                unifiedOrderRequestData.openid = OAuth2.fetchOpenid(merchantInfo.getAppid(), merchantInfo.getAppsecret(), code);
-                unifiedOrderRequestData.notify_url = getRequest().getRequestURL().substring(0, getRequest().getRequestURL().lastIndexOf("/") + 1) + CallbackAction.BRANDWCPAYCALLBACK;
+        SubMerchantUser subMerchantUser = SubMerchantUser.getSubMerchantUserById(Long.parseLong(subMerchantUserId));
+        if (subMerchantUser != null) {
+            SubMerchantInfo subMerchantInfo = SubMerchantInfo.getSubMerchantInfoById(subMerchantUser.getSubMerchantId());
+            if (subMerchantInfo != null) {
+                MerchantInfo merchantInfo = MerchantInfo.getMerchantInfoById(subMerchantInfo.getMerchantId());
+                if (merchantInfo != null) {
+                    UnifiedOrderRequestData unifiedOrderRequestData = new UnifiedOrderRequestData();
+                    unifiedOrderRequestData.appid = merchantInfo.getAppid();
+                    unifiedOrderRequestData.mch_id = merchantInfo.getMchId();
+                    unifiedOrderRequestData.sub_mch_id = subMerchantInfo.getSubId();
+                    unifiedOrderRequestData.body = getParameter("body").toString();
+                    unifiedOrderRequestData.total_fee = Integer.parseInt(getParameter("total_fee").toString());
+                    unifiedOrderRequestData.trade_type = "JSAPI";
+                    unifiedOrderRequestData.openid = OAuth2.fetchOpenid(merchantInfo.getAppid(), merchantInfo.getAppsecret(), code);
+                    unifiedOrderRequestData.notify_url = getRequest().getRequestURL().substring(0, getRequest().getRequestURL().lastIndexOf("/") + 1) + CallbackAction.BRANDWCPAYCALLBACK;
 
-                UnifiedOrder unifiedOrder = new UnifiedOrder(unifiedOrderRequestData);
-                if (!unifiedOrder.execute(merchantInfo.getApiKey())) {
-                    System.out.println("BrandWCPay Failed!");
-                    return AjaxActionComplete();
+                    UnifiedOrder unifiedOrder = new UnifiedOrder(unifiedOrderRequestData);
+                    if (!unifiedOrder.execute(merchantInfo.getApiKey())) {
+                        System.out.println("BrandWCPay Failed!");
+                        return AjaxActionComplete();
+                    }
+
+                    Map<String, String> map = new HashMap<>();
+                    map.put("appId", unifiedOrderRequestData.appid);
+                    map.put("timeStamp", String.valueOf(System.currentTimeMillis() / 1000));
+                    map.put("nonceStr", StringUtils.generateRandomString(32));
+                    map.put("package", "prepay_id=" + unifiedOrder.getResponseResult().get("prepay_id").toString());
+                    map.put("signType", "MD5");
+                    map.put("paySign", Signature.generateSign(map, merchantInfo.getApiKey()));
+                    return AjaxActionComplete(map);
                 }
-
-                Map<String, String> map = new HashMap<>();
-                map.put("appId", unifiedOrderRequestData.appid);
-                map.put("timeStamp", String.valueOf(System.currentTimeMillis() / 1000));
-                map.put("nonceStr", StringUtils.generateRandomString(32));
-                map.put("package", "prepay_id=" + unifiedOrder.getResponseResult().get("prepay_id").toString());
-                map.put("signType", "MD5");
-                map.put("paySign", Signature.generateSign(map, merchantInfo.getApiKey()));
-                return AjaxActionComplete(map);
             }
         }
 
@@ -148,25 +156,28 @@ public class PayAction extends AjaxActionSupport {
     }
 
     public String refund() throws Exception {
-        SubMerchantInfo subMerchantInfo = SubMerchantInfo.getSubMerchantInfoBySubId(getParameter("sub_mch_id").toString());
-        if (subMerchantInfo != null) {
-            MerchantInfo merchantInfo = MerchantInfo.getMerchantInfoById(subMerchantInfo.getMerchantId());
-            if (merchantInfo != null) {
-                RefundRequestData refundRequestData = new RefundRequestData();
-                refundRequestData.appid = merchantInfo.getAppid();
-                refundRequestData.mch_id = merchantInfo.getMchId();
-                refundRequestData.sub_mch_id = subMerchantInfo.getSubId();
-                refundRequestData.total_fee = Integer.parseInt(getParameter("total_fee").toString());
-                refundRequestData.refund_fee = Integer.parseInt(getParameter("refund_fee").toString());
-                refundRequestData.transaction_id = getParameter("transaction_id").toString();
-                refundRequestData.out_trade_no = getParameter("out_trade_no").toString();
-                refundRequestData.op_user_id = refundRequestData.mch_id;
-                Refund refund = new Refund(refundRequestData);
-                if (!refund.execute(merchantInfo.getApiKey())) {
-                    System.out.println("Refund Failed!");
-                    return AjaxActionComplete();
+        SubMerchantUser subMerchantUser = SubMerchantUser.getSubMerchantUserById(Long.parseLong(getParameter("id").toString()));
+        if (subMerchantUser != null) {
+            SubMerchantInfo subMerchantInfo = SubMerchantInfo.getSubMerchantInfoById(subMerchantUser.getSubMerchantId());
+            if (subMerchantInfo != null) {
+                MerchantInfo merchantInfo = MerchantInfo.getMerchantInfoById(subMerchantInfo.getMerchantId());
+                if (merchantInfo != null) {
+                    RefundRequestData refundRequestData = new RefundRequestData();
+                    refundRequestData.appid = merchantInfo.getAppid();
+                    refundRequestData.mch_id = merchantInfo.getMchId();
+                    refundRequestData.sub_mch_id = subMerchantInfo.getSubId();
+                    refundRequestData.total_fee = Integer.parseInt(getParameter("total_fee").toString());
+                    refundRequestData.refund_fee = Integer.parseInt(getParameter("refund_fee").toString());
+                    refundRequestData.transaction_id = getParameter("transaction_id").toString();
+                    refundRequestData.out_trade_no = getParameter("out_trade_no").toString();
+                    refundRequestData.op_user_id = refundRequestData.mch_id;
+                    Refund refund = new Refund(refundRequestData);
+                    if (!refund.execute(merchantInfo.getApiKey())) {
+                        System.out.println("Refund Failed!");
+                        return AjaxActionComplete();
+                    }
+                    return AjaxActionComplete(refund.getResponseResult());
                 }
-                return AjaxActionComplete(refund.getResponseResult());
             }
         }
 
