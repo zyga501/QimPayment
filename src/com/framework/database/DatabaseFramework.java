b2e2c4237957d036f9c4309d9516cca821f60393
Feng Chen <VonChenPlus@gmail.com>
2016-04-22 18:24:58 +0800

    update DatabaseFramework

--- src/com/framework/database/DatabaseFramework.java
+++ src/com/framework/database/DatabaseFramework.java
@@ -25,44 +25,65 @@ public abstract class DatabaseFramework {
 
     public <T> T selectOne(String var1) {
         SqlSession sqlSession = sqlSessionFactory().openSession();
-        T value = sqlSession.selectOne(var1);
-        sqlSession.close();
-        return value;
+        try {
+            T value = sqlSession.selectOne(var1);
+            return value;
+        }
+        finally {
+            sqlSession.close();
+        }
     }
 
     public <T> T selectOne(String var1, Object var2) {
         SqlSession sqlSession = sqlSessionFactory().openSession();
-        T value = sqlSession.selectOne(var1, var2);
-        sqlSession.close();
-        return value;
+        try {
+            T value = sqlSession.selectOne(var1, var2);
+            return value;
+        }
+        finally {
+            sqlSession.close();
+        }
     }
 
     public <E> List<E> selectList(String var1) {
         SqlSession sqlSession = sqlSessionFactory().openSession();
-        List<E> value = sqlSession.selectList(var1);
-        sqlSession.close();
-        return value;
+        try {
+            List<E> value = sqlSession.selectList(var1);
+            return value;
+        }
+        finally {
+            sqlSession.close();
+        }
     }
 
     public <E> List<E> selectList(String var1, Object var2) {
         SqlSession sqlSession = sqlSessionFactory().openSession();
-        List<E> value = sqlSession.selectList(var1, var2);
-        sqlSession.close();;
-        return value;
+        try {
+            List<E> value = sqlSession.selectList(var1, var2);
+            return value;
+        }
+        finally {
+            sqlSession.close();
+        }
     }
 
     public int insert(String var1, Object var2) {
         SqlSession sqlSession = sqlSessionFactory().openSession();
-        int value = sqlSession.insert(var1, var2);
-        sqlSession.commit();
-        sqlSession.close();
-        return value;
+        try {
+            int value = sqlSession.insert(var1, var2);
+            sqlSession.commit();
+            return value;
+        }
+        finally {
+            sqlSession.close();
+        }
     }
 
     public int insert(String var1, Object var2, Callable<Boolean> callable) {
         SqlSession sqlSession = sqlSessionFactory().openSession();
-        int value = sqlSession.insert(var1, var2);
+        int value = 0;
         try {
+            value = sqlSession.insert(var1, var2);
             if (callable == null || callable.call()) {
                 sqlSession.commit();
             }
@@ -71,19 +92,25 @@ public abstract class DatabaseFramework {
             }
         }
         catch (Exception exception) {
+            exception.printStackTrace();
             value = 0;
         }
-
-        sqlSession.close();
+        finally {
+            sqlSession.close();
+        }
         return value;
     }
 
     public int update(String var1, Object var2) {
         SqlSession sqlSession = sqlSessionFactory().openSession();
-        int value = sqlSession.update(var1, var2);
-        sqlSession.commit();
-        sqlSession.close();
-        return value;
+        try {
+            int value = sqlSession.update(var1, var2);
+            sqlSession.commit();
+            return value;
+        }
+        finally {
+            sqlSession.close();
+        }
     }
 
     protected abstract SqlSessionFactory sqlSessionFactory();
