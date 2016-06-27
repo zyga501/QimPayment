9d62aae9d51eec74999efdee391123b18198815a
zyga501 <x02540431>
2016-06-27 23:45:57 +0800

    remote prt

--- src/com/framework/socket/Server.java
+++ src/com/framework/socket/Server.java
@@ -1,11 +1,9 @@
 package com.framework.socket;
 import com.database.merchant.SubMerchantUser;
+import com.framework.utils.StringUtils;
 import net.sf.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -20,6 +18,17 @@ public class Server extends Thread{
     private static List<ServerThread> thread_list =new ArrayList<ServerThread>();
     private static ServerSocket serverSocket = null;
 
+    public static void main(String[] args){
+        Server server=null;
+        {
+            try {
+                server=new Server();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            server.start();
+        }
+    }
     public void run(){
         try {
             serverSocket = new ServerSocket(SERVER_PORT);
@@ -52,11 +61,12 @@ public class Server extends Thread{
         private PrintWriter out;
         private BufferedReader in;
         private Long id;
+        private String msgstr = "";
 
         public ServerThread(Socket s) throws IOException {
             client = s;
             System.out.println(client.getInetAddress().getHostAddress()+"  "+client.getPort());
-            out = new PrintWriter(client.getOutputStream(), true);
+            out = new PrintWriter (client.getOutputStream());
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
         }
 
@@ -64,12 +74,31 @@ public class Server extends Thread{
         public void run() {
             String jsonobj = "";
             try {
+                out.println("success");
+                out.flush();
                 jsonobj = in.readLine();//连接格式：{id:10920930293011}
                 String userid = JSONObject.fromObject(jsonobj).get("id").toString();
                 id = Long.parseLong(userid);
                 SubMerchantUser subMerchantUser = SubMerchantUser.getSubMerchantUserById(id);
-                if (null != subMerchantUser)
+                if (null != subMerchantUser) {
+                    for (ServerThread thread : thread_list) {
+                        if (null != thread.id && thread.id.equals(subMerchantUser.getId())) {
+                            thread.client.close();
+                            thread_list.remove(thread);
+                            thread = null;
+                            break;
+                        }
+                    }
                     thread_list.add(this);
+                }
+                while (true){
+                    if (!msgstr.equals("")){
+                        out.println(msgstr);
+                        msgstr = "";
+                        out.flush();
+                    }
+                    sleep(100);
+                }
             } catch (Exception e) {
                 try {
                     client.close();
@@ -81,8 +110,8 @@ public class Server extends Thread{
             }
         }
 
-        private void sendMessage(String msg) {
-            out.println(msg);
+        private void sendMessage(String msg) throws IOException {
+            msgstr = (msg);
         }
     }
 
@@ -90,8 +119,7 @@ public class Server extends Thread{
         if (thread_list.size() > 0)
             for (ServerThread thread : thread_list) {
                 if ( null!=thread.id && thread.id.equals(uid)) {
-                    thread.sendMessage(msg);
-                    thread.client.close();
+                        thread.sendMessage(msg);
                     break;
                 }
             }
