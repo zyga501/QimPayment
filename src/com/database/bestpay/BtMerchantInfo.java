472168e2ead4fa55f9f0e8da4ed59f74f11e49cd
unknown <VonChenPlus@gmail.com>
2016-08-10 20:34:49 +0800

    update bestpay database

--- src/com/database/bestpay/BtMerchantInfo.java
+++ src/com/database/bestpay/BtMerchantInfo.java
@@ -1,10 +1,10 @@
 package com.database.bestpay;
 
-public class MerchantInfo {
+public class BtMerchantInfo {
     public static void main(String[] args) throws Exception {
     }
 
-    public static MerchantInfo getMerchantInfoById(long id) {
+    public static BtMerchantInfo getMerchantInfoById(long id) {
         String statement = "com.database.bestpay.mapping.merchantInfo.getMerchantInfoById";
         return Database.Instance().selectOne(statement, id);
     }
@@ -25,6 +25,15 @@ public class MerchantInfo {
         this.mchId_ = mchId;
     }
 
+    public String getApiKey() {
+        return apiKey_;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey_ = apiKey;
+    }
+
     private Long id_;
     private String mchId_;
+    private String apiKey_;
 }
